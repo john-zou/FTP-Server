64764554147627cdfde0054a883bad1a910832a9
John Zou <john.hua.zou@gmail.com>
2020-03-31 21:17:08 -0700

    accept a connection

--- CSftp.c
+++ CSftp.c
@@ -1,35 +1,79 @@
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <stdio.h>
-#include "util.h"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
 #include "dir.h"
 #include "usage.h"
+#include "util.h"
 
 #define MIN_PORT 1024
 #define MAX_PORT 65535
 #define BUFFER_SIZE 1024
+#define QUEUE_SIZE 1
 
-// Here is an example of how to use the above function. It also shows
-// one how to get the arguments passed on the command line.
+typedef struct sockaddr_in addr;
 
-int main(int argc, char *argv[]) {
+addr getServerAddress(int port)
+{
+    addr address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+    return address;
+}
+
+void startListening(int port)
+{
+    debug("startListening()");
+    debug("creating socket");
+    int sockFD = socket(AF_INET, SOCK_STREAM, 0);
+    addr serverAddress = getServerAddress(port);
+    if (sockFD < 0)
+    {
+        debug("Socket creation failed!");
+    }
+    debug("created socket! ✓");
+    debug("sockFD: %d", sockFD);
+    if (bind(sockFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+    {
+        debug("Bind failed!");
+    }
+    debug("bind done! ✓");
+    debug("starting listen() ...");
+    listen(sockFD, QUEUE_SIZE);
+    debug("listen() returned! ✓");
+    // Prepare stuff for accepting client
+    addr clientAddress;
+    int sizeofClientAddress = sizeof(clientAddress);
+    debug("starting accept() ...");
+    int newSockFD = accept(sockFD, (struct sockaddr *)&clientAddress, &sizeofClientAddress);
+    debug("accept() returned with newSockFD = %d", newSockFD);
+    // TODO: Continue here
+}
+
+int main(int argc, char *argv[])
+{
     // Check the command line arguments
-    if (argc != 2) {
-      usage(argv[0]);
-      return -1;
+    if (argc != 2)
+    {
+        usage(argv[0]);
+        return -1;
     }
 
     int port = parseInt(argv[1]);
-    if (port < MIN_PORT || port > MAX_PORT) {
-      usage(argv[0]);
-      return -1;
+    if (port < MIN_PORT || port > MAX_PORT)
+    {
+        usage(argv[0]);
+        return -1;
     }
     debug("port: %d", port);
+    startListening(port);
 
-    // This is how to call the function in dir.c to get a listing of a directory.
-    // It requires a file descriptor, so in your code you would pass in the file descriptor 
-    // returned for the ftp server's data connection
-    
+    // This is how to call the function in dir.c to get a listing of a
+    // directory. It requires a file descriptor, so in your code you would pass
+    // in the file descriptor returned for the ftp server's data connection
+
     printf("Printed %d directory entries\n", listFiles(1, "."));
     return 0;
 }
